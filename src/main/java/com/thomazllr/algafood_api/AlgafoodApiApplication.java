38a6c5684ef2de854d7e2749bf1c9c0e09e0d33e
thomazllr <guithomaz270@gmail.com>
2025-07-14 16:56:50 -0300

    initial commit

---
+++ src/main/java/com/thomazllr/algafood_api/AlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.thomazllr.algafood_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApiApplication.class, args);
+	}
+
+}
