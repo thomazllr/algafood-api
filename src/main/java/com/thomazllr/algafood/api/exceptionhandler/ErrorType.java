5624bcfefb43cb600e787a8e9538605370e1f665
thomazllr <guithomaz270@gmail.com>
2025-07-21 20:47:04 -0300

    feat: resolvendo desafios de implementacao de exceptions

--- src/main/java/com/thomazllr/algafood/api/exceptionhandler/ErrorType.java
+++ src/main/java/com/thomazllr/algafood/api/exceptionhandler/ErrorType.java
@@ -7,8 +7,9 @@ public enum ErrorType {
 
     ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
     ERRO_DE_NEGOCIO("/erro-de-negocio", "Erro de negócio interno"),
-    ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso");
-
+    ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
 
     private String uri;
     private String title;
--- src/main/java/com/thomazllr/algafood/api/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/thomazllr/algafood/api/exceptionhandler/GlobalExceptionHandler.java
@@ -1,23 +1,30 @@
 package com.thomazllr.algafood.api.exceptionhandler;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
 import com.thomazllr.algafood.domain.exception.EntidadeEmUsoException;
 import com.thomazllr.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.thomazllr.algafood.domain.exception.NegocioException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
-
+    public ResponseEntity<Object> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         ErrorType errorType = ErrorType.ENTIDADE_NAO_ENCONTRADA;
         var detail = ex.getMessage();
@@ -27,8 +34,41 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
     }
 
+    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders
+            headers, HttpStatusCode status, WebRequest request) {
+
+        var path = ex.getPath().stream()
+                .map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
+
+        ErrorType errorType = ErrorType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = String.format("A propriedade '%s' recebeu um valor '%s' que é inválido. Corrija e informe o valor com o tipo %s.",
+                path,
+                ex.getValue(),
+                ex.getTargetType().getSimpleName());
+
+        Error error = criarErro(errorType, (HttpStatus) status, detail).build();
+
+        return handleExceptionInternal(ex, error, headers, status, request);
+
+    }
+
+    private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex, HttpHeaders
+            headers, HttpStatusCode status, WebRequest request) {
+
+        var path = ex.getPath().stream()
+                .map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
+
+        ErrorType errorType = ErrorType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = String.format("A propriedade '%s' não existe. Corrija ou remova essa propriedade e tente novamente.", path);
+
+        Error error = criarErro(errorType, (HttpStatus) status, detail).build();
+
+        return handleExceptionInternal(ex, error, headers, status, request);
+
+    }
+
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> handleNegocioException(NegocioException ex, WebRequest request) {
+    public ResponseEntity<Object> handleNegocioException(NegocioException ex, WebRequest request) {
 
         HttpStatus status = HttpStatus.BAD_REQUEST;
         ErrorType errorType = ErrorType.ERRO_DE_NEGOCIO;
@@ -40,7 +80,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
+    public ResponseEntity<Object> handleEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
 
         HttpStatus status = HttpStatus.CONFLICT;
         ErrorType errorType = ErrorType.ENTIDADE_EM_USO;
@@ -49,6 +89,60 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         var error = criarErro(errorType, status, detail).build();
 
         return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders
+            headers, HttpStatusCode status, WebRequest request) {
+
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if (rootCause instanceof InvalidFormatException invalidFormatException) {
+            return handleInvalidFormatException(invalidFormatException, headers, status, request);
+        }
+
+        if (rootCause instanceof PropertyBindingException propertyBindingException) {
+            return handlePropertyBindingException(propertyBindingException, headers, status, request);
+        }
+
+
+        ErrorType errorType = ErrorType.MENSAGEM_INCOMPREENSIVEL;
+        var detail = "O corpo da requisição está inválido. Verifique a sintaxe.";
+
+        var error = criarErro(errorType, (HttpStatus) status, detail).build();
+
+        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if (rootCause instanceof MethodArgumentTypeMismatchException methodArgumentTypeMismatchException) {
+            return handleMethodArgumentTypeMismatchException(methodArgumentTypeMismatchException, headers, status, request);
+        }
+
+        ErrorType errorType = ErrorType.PARAMETRO_INVALIDO;
+        var detail = "Algum parametro está errado.";
+
+        var error = criarErro(errorType, (HttpStatus) status, detail).build();
+
+        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
+    }
+
+    private ResponseEntity<Object> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, HttpHeaders
+            headers, HttpStatusCode status, WebRequest request) {
+
+        var path = ex.getPropertyName();
+
+        ErrorType errorType = ErrorType.PARAMETRO_INVALIDO;
+        String detail = String.format("A propriedade '%s' não pode.", path);
+
+        Error error = criarErro(errorType, (HttpStatus) status, detail).build();
+
+        return handleExceptionInternal(ex, error, headers, status, request);
+
     }
 
     @Override
@@ -68,9 +162,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                     .title(mensagem)
                     .status(status.value())
                     .build();
-        } else if (body instanceof String) {
+        } else if (body instanceof String corpo) {
             body = Error.builder()
-                    .title((String) body)
+                    .title(corpo)
                     .build();
         }
 
